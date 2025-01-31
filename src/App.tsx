6455e5e1a1ba00cd56832f3ad39a69bcc4bcd58a
webgalacticdev <webgalacticdev@example.com>
2025-01-31 19:33:45 +0200

    f

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/finconsult-build' : '/'}>
+      <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/finconsult' : '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/thank-you" element={<ThankYou />} />
